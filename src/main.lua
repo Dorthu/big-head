61dce05ce349f9d52f431e7c5bd34c73a250dd62
Will Smith <wsmith@linode.com>
2020-05-24 16:12:49 -0400

    Streetlights without collision

--- src/main.lua
+++ src/main.lua
@@ -2,11 +2,13 @@ import "CoreLibs/sprites"
 import "CoreLibs/graphics"
 import "nick"
 import "camera"
+import "orchestrator"
 
 local gfx <const> = playdate.graphics
 
 local nick = Nick()
 local camera = Camera(nick)
+spawnEnv()
 
 function playdate:update()
     gfx.clear()
--- src/orchestrator.lua
+++ src/orchestrator.lua
@@ -1,3 +1,32 @@
-function spawnEnv()
+import "CoreLibs/sprites"
+import "CoreLibs/graphics"
 
+local gfx <const> = playdate.graphics
+
+-- load up graphics
+local sprStreetlight <const> = gfx.image.new("images/streetlight")
+assert(sprStreetlight)
+
+local sprites = {}
+
+
+function spawnStreetlight(height, leftSide)
+    local streetlight = gfx.sprite.new()
+    local x = 94
+    local flip = gfx.kImageUnflipped
+    if not leftSide then
+        x = 306
+        flip = gfx.kImageFlippedX
+    end
+
+    streetlight:setImage(sprStreetlight, flip)
+    streetlight:moveTo(x, height)
+    streetlight:add()
+
+    return streetlight
+end
+
+function spawnEnv()
+    sprites[1] = spawnStreetlight(-20, true)
+    sprites[2] = spawnStreetlight(-140, false)
 end

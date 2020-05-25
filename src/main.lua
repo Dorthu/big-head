0005e5c26b01e4701294da960c081e271a1d801f
Will Smith <wsmith@linode.com>
2020-05-24 21:52:17 -0400

    Restart that game!

--- src/main.lua
+++ src/main.lua
@@ -9,24 +9,41 @@ local gfx <const> = playdate.graphics
 local sprLose <const> = gfx.image.new("images/tmp-lose")
 assert(sprLose)
 
--- initialization - TODO make this a function so we can restart the game
-local nick = Nick()
-local camera = Camera(nick)
-spawnEnv()
+local nick = nil
+local camera = nil
+
+function restartGame()
+    gfx.sprite.removeAll() -- clean slate plx
+    nick = Nick()
+    camera = Camera(nick)
+    spawnEnv()
+end
+
+local gameState = 1 -- 0 = ended, 1 = running
+restartGame()
 
 function playdate:update()
-    gfx.clear()
-    camera:update()
-    dx, dy = gfx.getDrawOffset()
-    gfx.sprite.update()
+    if gameState == 1 then
+        gfx.clear()
+        camera:update()
 
-    -- check for losing the game
-    local _, bottomY = gfx.getDrawOffset()
-    local _, nickY = nick:getPosition()
-    if nickY > (-1* bottomY) + 240 then lose() end
+        -- check for losing the game
+        local _, bottomY = gfx.getDrawOffset()
+        local _, nickY = nick:getPosition()
+        if nickY > (-1* bottomY) + 240 then lose() end
+    else
+        if playdate.buttonJustPressed(playdate.kButtonA) then
+            restartGame()
+            gameState = 1
+        end
+    end
+
+    -- always update sprites tho
+    gfx.sprite.update()
 end
 
 function lose()
+    gameState = 0
     s = gfx.sprite.new()
     s:setImage(sprLose)
     s:moveTo(200, 120)

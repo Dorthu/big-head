b724448a86ff3e8523cd19ff14c3489c4b3f603c
Will Smith <wsmith@linode.com>
2020-05-25 21:23:09 -0400

    Added birds!

--- src/orchestrator.lua
+++ src/orchestrator.lua
@@ -12,6 +12,11 @@ local sprGrass2 <const> = gfx.image.new("images/grass-2")
 assert(sprGrass1)
 assert(sprGrass2)
 
+local sprBird1 <const> = gfx.image.new("images/bird1")
+local sprBird2 <const> = gfx.image.new("images/bird2")
+assert(sprBird1)
+assert(sprBird2)
+
 local sprites = {}
 
 -- does what it says on the tin.
@@ -55,6 +60,41 @@ function spawnStreetlight(height, leftSide)
     return streetlight
 end
 
+function spawnBird(height, x)
+    local bird = gfx.sprite.new()
+    bird:setImage(sprBird1)
+    bird:moveTo(x, height)
+    bird:setCollideRect(11, 19, 38, 12)
+    bird:setGroups({2})
+    bird:add()
+    bird.direction = 1
+    bird.counter = 1
+
+    function bird:update()
+        local x, y = self:getPosition()
+        local needsUpdate = false
+        if x > 350 then
+            self.direction = -1
+            needsUpdate = true
+        elseif x < 50 then
+            self.direction = 1
+            needsUpdate = true
+        end
+        self:moveTo(x+(5*self.direction), y)
+        self.counter += 1
+        if needsUpdate or self.counter == 11 or self.counter == 21 then
+            local flip = gfx.kImageUnflipped
+            if self.direction < 0 then flip = gfx.kImageFlippedX end
+            if self.counter >= 11 and self.counter < 21 then
+                self:setImage(sprBird2, flip)
+            else
+                self:setImage(sprBird1, flip)
+                self.counter = 1
+            end
+        end
+    end
+end
+
 -- spawns the entire level.  This uses some randomness to always give you a new
 -- adventure.
 function spawnEnv()
@@ -64,9 +104,17 @@ function spawnEnv()
     makeGround()
     local space = 100
 
+    -- spawn the streetlight region
     for c=1,5 do
-        local thisSpace = (c * 120) + math.random(0, 70)
+        local thisSpace = (c * 60) + math.random(0, 40)
         spawnStreetlight(-1 * (space + thisSpace), c % 2 == 0)
-        space = thisSpace
+        space += thisSpace
+    end
+
+    -- spawn the bird region
+    for c=1,5 do
+        local thisSpace = (c * 70) + math.random(-30, 50)
+        spawnBird(-1 * (space + thisSpace), math.random(0, 300))
+        space += thisSpace
     end
 end
